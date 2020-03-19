3d190cfade3a8e35b5cdf282bb4cf955c07dc79d
aryaman-titan <aryamanbehera@gmail.com>
2020-03-20 03:35:00 +0530

    keygen.py added

---
+++ keygen.py
@@ -0,0 +1,32 @@
+from pwn import *
+
+# Declare a list containing all alphabets and '@' key
+
+alpha = 'a'
+alphabets = []
+
+for i in range(0,26):
+    alphabets.append(alpha)
+    alpha = chr(ord(alpha) + 1)
+
+alphabets.append('@')
+
+# Generating the key
+
+key = random.choice(alphabets)
+key += chr(0x40) # chr(0x40) = '@'
+key += random.choice(alphabets)
+key += random.choice(alphabets)
+key += chr(300 - (ord(key[2])+ord(key[3])))
+
+# Get the present working directory of the file so that it 
+# works in any directory where our binary exists
+
+shell = process('sh')
+shell.sendline('pwd')
+pwd = shell.recvline(keepends=False)
+io = process([pwd+'/glowwine',key])
+print("Using key : ", key)
+output = io.recvline()
+print(output)
+print("Congrats..binary cracked!! :-)")
